bb91f8699347f23da03bc3dda6bbd56395f02f06
jws3873 <95128838+jws3873@users.noreply.github.com>
2024-10-13 22:30:37 +0900

    [level 1] Title: 폰켓몬, Time: 4.33 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -7,11 +7,7 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], nums[i]);
         }
-        if ((int)nums.length/2 > map.size()) {
-            answer = map.size();
-        }else {
-            answer = (int)nums.length/2;
-        }
+        answer = Math.min(map.size(), nums.length/2);
         return answer;
     }
 }
